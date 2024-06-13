2d89eef5906a0d7e8d2dd088c5927830150c079b
Hiel Saraiva Freitas de Queiroga <hielsaraiva11.hs@gmail.com>
2024-06-13 09:49:00 -0300

    payment, one-to-one association

--- src/main/java/com/hielsaraiva/project/config/TestConfig.java
+++ src/main/java/com/hielsaraiva/project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.hielsaraiva.project.entities.Category;
 import com.hielsaraiva.project.entities.Order;
 import com.hielsaraiva.project.entities.OrderItem;
+import com.hielsaraiva.project.entities.Payment;
 import com.hielsaraiva.project.entities.Product;
 import com.hielsaraiva.project.entities.User;
 import com.hielsaraiva.project.entities.enums.OrderStatus;
@@ -82,5 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
