667497d3c3b4ef52d156045af1270c461e372902
Hiel Saraiva Freitas de Queiroga <hielsaraiva11.hs@gmail.com>
2024-06-13 13:59:52 -0300

    exception handling - update

--- src/main/java/com/hielsaraiva/project/services/UserService.java
+++ src/main/java/com/hielsaraiva/project/services/UserService.java
@@ -13,6 +13,8 @@ import com.hielsaraiva.project.repositories.UserRepository;
 import com.hielsaraiva.project.services.exceptions.DatabaseException;
 import com.hielsaraiva.project.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
